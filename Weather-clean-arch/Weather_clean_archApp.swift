439da54e42424ef87c2f05ded4f2bba0cfca93d3
Martin Kolomazník <kolomaznik.martin@outlook.com>
2021-11-29 15:45:50 +0100

    Initial Commit

---
+++ Weather-clean-arch/Weather_clean_archApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_clean_archApp.swift
+//  Weather-clean-arch
+//
+//  Created by Martin on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Weather_clean_archApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
